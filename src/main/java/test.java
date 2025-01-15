5d661fe9922c38a6ca37ca2aaefb602edb90eab6
Jinse <akop4869@gmail.com>
2025-01-15 19:45:14 +0900

    20250115 : BOJ1913

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,9 +3,13 @@ import java.util.List;
 
 public class test {
     public static void main(String arg[]) {
-        List<int[]> testList = new ArrayList<>();
-        testList.add(new int[100]);
-        for (int i : testList.get(0)) {
+//        List<int[]> testList = new ArrayList<>();
+//        testList.add(new int[100]);
+//        for (int i : testList.get(0)) {
+//            System.out.println(i);
+//        }
+        int [] test = new int[4];
+        for (int i : test) {
             System.out.println(i);
         }
     }
