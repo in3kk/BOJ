9afc4900600a05c7d133c811d89735b4e6b1385c
Jinse <akop4869@gmail.com>
2025-02-05 18:13:25 +0900

    20250205

--- src/main/java/완전탐색/BOJ12919.java
+++ src/main/java/완전탐색/BOJ12919.java
@@ -8,43 +8,30 @@ import java.util.Collections;
 import java.util.List;
 
 public class BOJ12919 {
+    static String S;
+    static String T;
+    static int answer = 0;
     public static void main(String[] arg) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String s = br.readLine();
-        String t = br.readLine();
-        String [] array_t = t.split("");
-        List<String> list_t = new ArrayList<>();
-        for (int x = 0; x < t.length(); x++) {
-            list_t.add(array_t[x]);
-        }
-        for (int x = s.length(); x < t.length(); x++) {
-            String lastIndex = list_t.get(list_t.size()-1);
-            String beginIndex = list_t.get(0);
-            if (beginIndex.equals("B")) {
-                if (lastIndex.equals("B")) {
-                    Collections.reverse(list_t);
-                    list_t.remove(list_t.size()-1);
-                } else if (lastIndex.equals("A")) {
-                    list_t.remove(list_t.size()-1);
-                }
-            } else if (beginIndex.equals("A")) {
-                if (lastIndex.equals("A")) {
-                    list_t.remove(list_t.size()-1);
-                } else if (lastIndex.equals("B")) {
-                    list_t.remove(list_t.size()-1);
-                }
+        S = br.readLine();
+        T = br.readLine();
+
+        dfs(T);
+        System.out.println(answer);
+    }
+
+    public static void dfs(String t) {
+        if (t.length() == S.length()) {
+            if (S.equals(t)) {
+                answer = 1;
             }
-            for (String s1 : list_t) {
-                System.out.print(s1);
-            }
-            System.out.println();
+            return;
         }
-        String tmp = String.join("",list_t);
-        if (tmp.equals(s)) {
-            System.out.println(1);
-        } else {
-            System.out.println(0);
+        if (t.endsWith("A")) {
+            dfs(t.substring(0,t.length()-1));
         }
-        System.out.println(tmp);
+        if (t.startsWith("B")) {
+            dfs(new StringBuilder(t.substring(1)).reverse().toString());
+        }
     }
 }
