235c02c1493142590e98ae543e331786eba0d242
in3kk <akop4869@gmail.com>
2025-03-17 21:07:57 +0900

    20250317:BOJ10775

---
+++ src/main/java/분리집합/BOJ10775.java
@@ -0,0 +1,53 @@
+package 분리집합;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ10775 {
+    public static int g, p;
+    public static int[] parent;
+    public static void main(String[] argc) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        g = Integer.parseInt(br.readLine());
+        p = Integer.parseInt(br.readLine());
+
+        parent = new int[g + 1];
+
+        for (int i = 0; i <= g ; i++) {
+            parent[i] = i;
+        }
+
+        int count = 0;
+        for (int i = 0; i < p ; i++) {
+            int now = Integer.parseInt(br.readLine());
+
+            int p = find(now);
+            if(p != 0){
+                union(p, p - 1);
+                count++;
+            }
+            else
+                break;
+        }
+
+        System.out.println(count);
+    }
+
+
+    public static int find(int now) {
+        if(now == parent[now])
+            return now;
+        return parent[now] = find(parent[now]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if(x != y) {
+            parent[x] = y;
+        }
+    }
+}
